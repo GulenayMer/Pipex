860f7d741312aa0e541a769289198c270a60d82f
GulenayMer <61357481+GulenayMer@users.noreply.github.com>
2022-05-11 21:02:27 +0200

    Update pipex.h

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: mgulenay <mgulenay@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 14:57:56 by mgulenay          #+#    #+#             */
-/*   Updated: 2022/04/26 21:48:54 by mgulenay         ###   ########.fr       */
+/*   Updated: 2022/05/09 11:40:43 by mgulenay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,15 @@
 # include <sys/wait.h>
 # include <fcntl.h>
 # include <stdio.h>
+# include "libft/libft.h"
 
-
+void	first_process(int *pipefd, int *fds, char **argv, char **envp);
+void	second_process(int *pipefd, int *fds, char **argv, char **envp);
+char	*find_right_path(char *command, char **envp);
+void	pipex(int *fds, int *pipefd, char **argv, char **envp);
+void	ft_free(char **str);
+void	error_message(int argc);
+void	command_execution(char **command, char *path, char **envp);
+char	**env_path(char **envp);
 
 #endif
